02671f48d891f5547b6a2b9d2e64c14898145d8b
girassol2023 <123131164+girassol2023@users.noreply.github.com>
2023-03-14 19:29:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+canvas =
+document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 1;
+ctx.rect(150,143,430,200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.rect(250,210,40,0,2 * Math.PI);
+ctx.stroke();
